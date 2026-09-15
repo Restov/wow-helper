8aafe27a147900000412aebd9e450e04b01db2f3
Restov <34414477+Restov@users.noreply.github.com>
2026-09-15 18:25:21 +0700

    test pipelines

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,7 @@ import { createApp } from "./app";
 
 const port = Number(Bun.env.PORT ?? 3000);
 
+
 const app = createApp().listen(port);
 
 console.log(`API is running at http://${app.server?.hostname}:${app.server?.port}`);
